9ac9ab56e97d45d75f3c0f2be496856d071bbba7
Replit user <>
2024-01-15 16:39:14 -0600

    first commit

--- index.js
+++ index.js
@@ -1,32 +1,67 @@
-// index.js
-// where your node app starts
+const express = require("express");
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+absolutePath = __dirname + "/views/index.html";
+app.get("/", (req, res) => {
+  res.sendFile(absolutePath);
 });
 
+app.use(express.static(__dirname + "/public"));
 
-// your first API endpoint... 
+const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Spt",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+app.get(
+  "/api/:word",
+  (req, res, next) => {
+    if (req.params.word.includes("-")) {
+      let dateString = req.params.word.toString();
+      req.unix = parseInt(new Date(dateString).getTime());
+      req.utc = new Date(dateString).toUTCString();
+      next();
+    } else {
+      let dateInt = parseInt(req.params.word);
+      let dateObj = new Date(dateInt);
+      let dayy = dateObj.getUTCDay();
+      let day = DAYS[dayy];
+      let date = dateObj.getUTCDate();
+      let monthh = dateObj.getUTCMonth();
+      let month = MONTHS[monthh];
+      let year = dateObj.getUTCFullYear();
+      let hours = dateObj.getUTCHours();
+      let minutes = dateObj.getUTCMinutes();
+      let seconds = dateObj.getUTCSeconds();
+      let finalDate = `${day}, ${date} ${month} ${year} ${hours}:${minutes}:${seconds} GMT`;
+      req.utc = finalDate;
+      req.unix = parseInt(req.params.word);
+      next();
+    }
+  },
+  (req, res) => {
+    res.json({ unix: req.unix, utc: req.utc });
+  },
+);
+
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
